452d9f6f87b092d1666d7462829c7e9270cf5711
sgs12-star <sgs12@aluno.ifal.edu.br>
2026-06-18 18:05:39 -0300

    domain de filmes populares e historico da sylvia

---
+++ lib/domain/popular_historico.dart
@@ -0,0 +1,23 @@
+class Popular {
+  late String titulo;
+
+  Popular ({
+    required this.titulo;
+  });
+
+  Popular.fromJson(Map<String, dynamic> json){
+    titulo = json['titulo'];
+  }
+}
+
+class Historico {
+  late String termo;
+
+  Historico ({
+    required this.termo;
+  });
+
+  Historico.fromJson(Map<String, dynamic> json){
+    termo = json['termo'];
+  }
+}
