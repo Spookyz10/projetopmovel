b55fa3970031b4826a1e5eb5311cff9c8a39b313
sgs12-star <sgs12@aluno.ifal.edu.br>
2026-06-18 18:34:46 -0300

    historico dao

---
+++ lib/db/historico_dao.dart
@@ -0,0 +1,3 @@
+import 'package:project_c/db/db_helper.dart';
+import 'package:project_c/domain/popular_historico.dart';
+import 'package:sqflite/sqflite.dart';
---
+++ lib/db/popular_dao.dart
@@ -0,0 +1,4 @@
+import 'package:project_c/db/db_helper.dart';
+import 'package:project_c/domain/popular_historico.dart';
+import 'package:sqflite/sqflite.dart';
+
