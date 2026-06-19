cf5b8c17223c7aa8bce752c13639c5d95caf9cb1
sgs12-star <sgs12@aluno.ifal.edu.br>
2026-06-19 11:46:58 -0300

    historico e popular dao

--- lib/db/historico_dao.dart
+++ lib/db/historico_dao.dart
@@ -1,3 +1,31 @@
 import 'package:project_c/db/db_helper.dart';
 import 'package:project_c/domain/popular_historico.dart';
 import 'package:sqflite/sqflite.dart';
+
+class HistoricoDao {
+  Future<void> inserirHistorico(Historico historico) async {
+    Database db = await DBHelper().initDB();
+    await db.insert(
+      'HISTORICO',
+      {'termo': historico.termo},
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<List<Historico>> listarHistorico() async{
+    Database db = await DBHelper().initDB();
+
+    var listaResult = await db.rawQuery('SELECT * FROM HISTORICO;');
+
+    List<Historico> listaHistorico = [];
+    for (var json in listaResult) {
+      listaHistorico.add(Historico.fromJson(json));
+    }
+    return listaHistorico;
+  }
+
+  Future<void> limparHistorico() async {
+    Database db = await DBHelper().initDB();
+    await db.delete('HISTORICO');
+  }
+}
--- lib/db/popular_dao.dart
+++ lib/db/popular_dao.dart
@@ -2,3 +2,17 @@ import 'package:project_c/db/db_helper.dart';
 import 'package:project_c/domain/popular_historico.dart';
 import 'package:sqflite/sqflite.dart';
 
+class PopularDao {
+  Future<List<Popular>> listarPopular() async {
+    Database db = await DBHelper().initDB();
+    
+    var listaResult = await db.rawQuery('SELECT * FROM POPULAR;');
+
+    List<Popular> listaPopular = [];
+    for (var json in listaResult){
+      Popular popular = Popular.fromJson(json);
+      listaPopular.add(popular);
+    }
+    return listaPopular;
+  }
+}
